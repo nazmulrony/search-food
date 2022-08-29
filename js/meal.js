6e2ab0fa391b3bc5ffb29f9d98b2a8e8bae44459
nazmulrony <nhrony65@gmail.com>
2022-08-29 20:30:38 +0600

    modal added

--- js/meal.js
+++ js/meal.js
@@ -8,11 +8,11 @@ const loadMeal = (search) => {
 const displayFood = (foods) => {
     const foodContainer = document.getElementById('food-container');
     foods.forEach(food => {
-        console.log(food);
+        // console.log(food);
         const foodDiv = document.createElement('div');
         foodDiv.classList.add('col');
         foodDiv.innerHTML = `
-        <div class="card">
+        <div onclick="loadDetails(${food.idMeal})" class="card" data-bs-toggle="modal" data-bs-target="#exampleModal">
             <img src="${food.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${food.strMeal}</h5>
@@ -30,6 +30,44 @@ const searchMeal = () => {
     const searchItem = searchElement.value;
     loadMeal(searchItem);
     searchElement.value = '';
+}
+
+const loadDetails = (foodId) => {
+    url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayDetails(data.meals[0]));
+}
+const displayDetails = (foodData) => {
+    const detailContainer = document.getElementById('detail-container');
+    detailContainer.innerHTML = `
+   <div class="modal-body">
+   <div class="mb-3">
+   <div class=" row g-2">
+       <div class="col-md-4">
+           <img src="${foodData.strMealThumb}" class="img-fluid rounded" alt="...">
+       </div>
+       <div class="col-md-8">
+           <div class="card-body">
+               <h2 class="card-title">${foodData.strMeal}</h2>
+               <h5 class="">Food Category: ${foodData.strCategory}</h5>
+               <h5 class="">Food Region: ${foodData.strArea}</h5>
+               
+               <p class="card-text">Instruction: ${foodData.strInstructions.slice(0, 400)}</p>
+               
+           </div>
+       </div>
+   </div>
+</div>
+    
+  </div>
+    </div>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
+    </div>
+    `
+
+    console.log(foodData);
 }
 
 loadMeal('chicken');
